b6dfb5c79aaa6c0d31c2d57a112903a113146391
ksb2233 <2233ksb@naver.com>
2021-02-02 16:08:10 +0900

    lint 수정

--- projects/kim/src/app/main/main.module.ts
+++ projects/kim/src/app/main/main.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainComponent } from './main.component';
+import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { MainComponent } from './main.component';
 
 @NgModule({
   declarations: [
