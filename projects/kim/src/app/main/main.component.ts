b075a86f5ce881a249fca826f9b192ed59331766
ksb2233 <2233ksb@naver.com>
2021-02-07 21:36:50 +0900

    mainComponent providers 로 counterService 인스턴스 생성

--- projects/kim/src/app/main/main.component.ts
+++ projects/kim/src/app/main/main.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import {
-  CounterService
-} from '../core';
 
 @Component({
   selector: 'app-main',
-  templateUrl: './main.component.html',
-  providers: [CounterService]
+  templateUrl: './main.component.html'
 })
 export class MainComponent { }
--- projects/kim/src/app/main/main.module.ts
+++ projects/kim/src/app/main/main.module.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {
+  CounterService
+} from '../core/services/counts.service';
 import { SharedModule } from '../shared/shared.module';
 import { MainComponent } from './main.component';
 
@@ -17,5 +20,8 @@ const routes: Routes = [
     SharedModule,
     RouterModule.forChild(routes)
   ],
+  providers: [
+    CounterService
+  ]
 })
 export class MainModule { }
