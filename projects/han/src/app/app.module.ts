1e7fc4edbb37cfb7652d96e432469e264ed39620
dongjun.han <dongjun.han@owaysoft.com>
2021-02-08 14:24:05 +0900

    lint 수정 필요 소스 반영

--- projects/han/src/app/app.module.ts
+++ projects/han/src/app/app.module.ts
@@ -4,8 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
-
-
 @NgModule({
   declarations: [
     AppComponent
