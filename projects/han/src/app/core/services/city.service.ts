0da341e52cb017b7100c8de8908a0fa827e5bfda
dongjun.han <dongjun.han@owaysoft.com>
2021-02-16 11:34:30 +0900

    city service에 json 변환 로직 추가

--- projects/han/src/app/core/services/city.service.ts
+++ projects/han/src/app/core/services/city.service.ts
@@ -1,28 +1,38 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { City } from '../models';
+import { City, MOCK_CITY } from '../models';
 import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
-  private citySubject: BehaviorSubject<City> = new BehaviorSubject<City>({title: '', woeid: 0});
+  private citySubject: BehaviorSubject<City> = new BehaviorSubject<City>({ title: '', woeid: 0 });
   private city$: Observable<City> = this.citySubject.asObservable();
 
   constructor(private apiService: ApiService) { }
 
-  fetchCity(cityName: string): Observable<City> {
+  public fetchCity(cityName: string): Observable<City> {
+    /*
     this.apiService
       .get(`/api/location/search/?query=${cityName}`)
       .subscribe(data => {
         this.citySubject.next({title: data.title, woeid: data.woeid});
       });
+    */
+    this.citySubject.next(this.fromJsonToCity(MOCK_CITY));
 
     return this.city$;
   }
 
-  getCurrentCity(): Observable<City> {
+  public getCurrentCity(): Observable<City> {
     return this.city$;
+  }
+
+  private fromJsonToCity(json: any): City {
+    return {
+      title: json.title,
+      woeid: json.woeid
+    } as City;
   }
 }
