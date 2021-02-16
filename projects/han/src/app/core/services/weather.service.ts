c64731bdff6a762808a92a3bd4952823fd724e93
dongjun.han <dongjun.han@owaysoft.com>
2021-02-16 11:45:36 +0900

    weather service fetchWeather 수정 및 변환로직 추가

--- projects/han/src/app/core/services/weather.service.ts
+++ projects/han/src/app/core/services/weather.service.ts
@@ -1,44 +1,57 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { WeatherCondition } from '../enums';
-import { Weather } from '../models';
+import { MOCK_WEATHER, Weather } from '../models';
 import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-  private weatherSubject: BehaviorSubject<Weather> = new BehaviorSubject<Weather>({
-    condition: WeatherCondition.unknown,
-    formattedCondition: '',
-    temp: 0,
-    minTemp: 0,
-    maxTemp: 0,
-    locationId: 0,
-    lastUpdated: new Date(),
-    location: '',
-  });
-  private weather$: Observable<Weather> = this.weatherSubject.asObservable();
+  private weatherSubject: BehaviorSubject<Weather>;
+  private weather$: Observable<Weather>;
 
-  constructor(
-    private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {
+    this.weatherSubject = new BehaviorSubject<Weather>({
+      condition: null,
+      formattedCondition: '',
+      temp: 0,
+      minTemp: 0,
+      maxTemp: 0,
+      locationId: 0,
+      lastUpdated: null,
+      location: ''
+    });
+    this.weather$ = this.weatherSubject.asObservable();
+  }
 
-  fetchWeather(locationId: string): Observable<Weather> {
+  public fetchWeather(locationId: string): Observable<Weather> {
+    /*
     this.apiService
       .get(`/api/location/${locationId}`)
       .subscribe(weather => {
         this.weatherSubject.next(this.fromJsonToWeather(weather));
       });
+    */
+    this.weatherSubject.next(this.fromJsonToWeather(MOCK_WEATHER));
     return this.weather$;
   }
 
-  private fromJsonToWeather(json: any) {
+  private fromJsonToWeather(json: any): Weather {
+    const consolidatedWeather = json.consolidated_weather[0];
     return {
-
+      condition: this.mapStringToWeatherCondition(consolidatedWeather.weather_state_abbr),
+      formattedCondition: consolidatedWeather.weather_state_name,
+      temp: consolidatedWeather.the_temp,
+      minTemp: consolidatedWeather.min_temp,
+      maxTemp: consolidatedWeather.max_temp,
+      locationId: json.woeid,
+      lastUpdated: new Date(),
+      location: json.title
     } as Weather;
   }
 
-  private mapStringToWeatherCondition(input: string) {
+  private mapStringToWeatherCondition(input: string): WeatherCondition {
     let state: WeatherCondition;
     switch (input) {
       case 'sn':
