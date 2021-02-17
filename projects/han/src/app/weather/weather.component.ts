12156d333efd59f77134e79a7703da459849a2da
dongjun.han <dongjun.han@owaysoft.com>
2021-02-17 13:37:46 +0900

    observable 객체가 아닌 weather 데이터 뿌리도록 변경 및 마크업 변경

--- projects/han/src/app/weather/weather.component.ts
+++ projects/han/src/app/weather/weather.component.ts
@@ -1,22 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Weather } from '../core/models';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
-export class WeatherComponent implements OnInit {
-
-  weather$: Observable<Weather>;
-
-  constructor(
-    private route: ActivatedRoute
-  ) { }
-
-  ngOnInit(): void {
-    console.log('weathercomponent', this.route.data);
-  }
-}
+export class WeatherComponent { }
--- projects/han/src/app/weather/weatherinfo/weatherinfo.component.ts
+++ projects/han/src/app/weather/weatherinfo/weatherinfo.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { environment } from 'projects/han/src/environments/environment';
-import { Observable } from 'rxjs';
 import { Weather } from '../../core/models';
-import { WeatherService } from '../../core/services/weather.service';
 
 @Component({
   selector: 'app-weatherinfo',
@@ -11,13 +10,14 @@ import { WeatherService } from '../../core/services/weather.service';
 })
 export class WeatherinfoComponent implements OnInit {
 
-  weather$: Observable<Weather>;
-
+  weather: Weather = {} as Weather;
   imageUrl = `${environment.api_url}/static/img/weather/png/64/`;
 
-  constructor(private weatherService: WeatherService) { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.weather$ = this.weatherService.getCurrentWeather();
+    this.route.data.subscribe(data => {
+      this.weather = data.weather;
+    });
   }
 }
