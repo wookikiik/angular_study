885995a2047ef94713d89afa4887388bdf2494a8
dongjun.han <dongjun.han@owaysoft.com>
2021-02-18 16:22:11 +0900

    weather image 디렉티브 적용 임시추가

--- projects/han/src/app/weather/weatherinfo/weatherinfo.component.ts
+++ projects/han/src/app/weather/weatherinfo/weatherinfo.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from 'projects/han/src/environments/environment';
+import { WeatherCondition } from '../../core/enums';
 import { Weather } from '../../core/models';
 
 @Component({
@@ -15,4 +16,8 @@ export class WeatherinfoComponent {
   imageUrl = `${environment.image_api_url}/static/img/weather/png/64/`;
 
   constructor(private route: ActivatedRoute) { }
+
+  mapConditionToImage(condition: WeatherCondition): string {
+    return '';
+  }
 }
