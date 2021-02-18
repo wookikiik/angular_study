c937de31ad0fdaa7c88f3f5143b9d94ab2b8fb9a
dongjun.han <dongjun.han@owaysoft.com>
2021-02-18 11:54:51 +0900

    null이 아닌 weather 1번만 가져오게 수정

--- projects/han/src/app/weather/weather.resolver.ts
+++ projects/han/src/app/weather/weather.resolver.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { switchMap, take } from 'rxjs/operators';
+import { filter, switchMap, take } from 'rxjs/operators';
 import { Weather } from '../core/models';
 import { CityService } from '../core/services/city.service';
 import { WeatherService } from '../core/services/weather.service';
@@ -18,7 +18,10 @@ export class WeatherResolver implements Resolve<Weather> {
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Weather> {
         return this.cityService.getCurrentLocationId().pipe(
             switchMap(locationId => {
-                return this.weatherService.fetchWeather(locationId).pipe(take(2));
+                return this.weatherService.fetchWeather(locationId).pipe(
+                    filter(weather => weather != null),
+                    take(1)
+                );
             }),
         );
     }
