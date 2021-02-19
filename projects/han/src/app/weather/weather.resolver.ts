3cc79c9aa877832bf60059283485b6bfba5f9469
dongjun.han <dongjun.han@owaysoft.com>
2021-02-19 12:30:28 +0900

    로직 단순화

--- projects/han/src/app/weather/weather.resolver.ts
+++ projects/han/src/app/weather/weather.resolver.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { filter, switchMap, take } from 'rxjs/operators';
+import { take } from 'rxjs/operators';
 import { Weather } from '../core/models';
 import { CityService } from '../core/services/city.service';
 import { WeatherService } from '../core/services/weather.service';
@@ -16,13 +16,8 @@ export class WeatherResolver implements Resolve<Weather> {
     ) { }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Weather> {
-        return this.cityService.getCurrentLocationId().pipe(
-            switchMap(locationId => {
-                return this.weatherService.fetchWeather(locationId).pipe(
-                    filter(weather => weather != null),
-                    take(1)
-                );
-            }),
-        );
+        return this.weatherService
+            .fetchWeather(this.cityService.getCurrentLocationId())
+            .pipe(take(1));
     }
 }
