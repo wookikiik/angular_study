5847f3174fd98f319f81bf81c372e22e1fabc035
dongjun.han <dongjun.han@owaysoft.com>
2021-02-19 16:10:03 +0900

    angular material module 추가

--- projects/han/src/app/app.module.ts
+++ projects/han/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -14,7 +15,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/han/src/app/shared/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonToggleModule
+  ],
+  exports: [
+    MatButtonToggleModule
+  ]
+})
+export class MaterialModule { }
