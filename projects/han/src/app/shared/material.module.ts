2f7d9e280b8ead171887138786f87109435162d6
dongjun.han <dongjun.han@owaysoft.com>
2021-02-22 11:45:52 +0900

    material button 적용

--- projects/han/src/app/shared/material.module.ts
+++ projects/han/src/app/shared/material.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [],
   imports: [
+    MatButtonModule,
     MatButtonToggleModule
   ],
   exports: [
+    MatButtonModule,
     MatButtonToggleModule
   ]
 })
